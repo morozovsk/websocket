b8917d62e6169a8182fc44d42df05cf7a09e888f
morozovsk <morozovsk@gmail.com>
2014-03-15 23:51:53 +0400

    bug fix

--- samples/yii/protected/config/console.php
+++ samples/yii/protected/config/console.php
@@ -16,6 +16,7 @@ return array(
                 'class' => 'WebsocketMasterHandler',
                 'socket' => 'tcp://127.0.0.1:8001',// unix:///tmp/mysock
                 'workers' => 1,
+                'pid' => '/tmp/websocket2.pid',
             ),*/
             /*'worker' => array(
                 'socket' => 'tcp://127.0.0.1:8000',
