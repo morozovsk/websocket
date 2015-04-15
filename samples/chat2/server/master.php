ad5926c8fea78df0b6db7bfeff5f41699205eaa3
Vladimir Goncharov <morozovsk@gmail.com>
2015-04-16 01:42:32 +0300

    Update master.php

--- samples/chat2/server/master.php
+++ samples/chat2/server/master.php
@@ -8,7 +8,7 @@ if (empty($argv[1]) || !in_array($argv[1], array('start', 'stop', 'restart'))) {
 $config = array(
     'class' => 'Chat2WebsocketMasterHandler',
     'pid' => '/tmp/websocket_chat2_master.pid',
-    'websocket' => 'tcp://127.0.0.1:8000',
+    'websocket' => 'tcp://127.0.0.1:8011',
     'localsocket' => 'tcp://127.0.0.1:8010',
     //'master' => 'tcp://127.0.0.1:8020',
     //'eventDriver' => 'event'
