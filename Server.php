b365fd39203966b845e372bdbb782dc7d371b202
Vladimir Goncharov <morozovsk@gmail.com>
2016-06-21 16:13:48 +0300

    Update Server.php

--- Server.php
+++ Server.php
@@ -25,7 +25,7 @@ class Server
         $server = $service = $master = null;
 
         if (!empty($this->config['websocket'])) {
-            //открываем серверный сокет
+            //open server socket
             $server = stream_socket_server($this->config['websocket'], $errorNumber, $errorString);
             stream_set_blocking($server, 0);
 
@@ -35,7 +35,7 @@ class Server
         }
 
         if (!empty($this->config['localsocket'])) {
-            //создаём сокет для обработки сообщений от скриптов
+            //create a socket for the processing of messages from scripts
             $service = stream_socket_server($this->config['localsocket'], $errorNumber, $errorString);
             stream_set_blocking($service, 0);
 
@@ -45,7 +45,7 @@ class Server
         }
 
         if (!empty($this->config['master'])) {
-            //создаём сокет для обработки сообщений от скриптов
+            //create a socket for the processing of messages from slaves
             $master = stream_socket_client($this->config['master'], $errorNumber, $errorString);
             stream_set_blocking($master, 0);
 
@@ -64,13 +64,13 @@ class Server
 
         file_put_contents($this->config['pid'], posix_getpid());
 
-        //list($pid, $master, $workers) = $this->spawnWorkers();//создаём дочерние процессы
+        //list($pid, $master, $workers) = $this->spawnWorkers();//create child processes
 
         /*if ($pid) {//мастер
             file_put_contents($this->config['pid'], $pid);
-            fclose($server);//мастер не будет обрабатывать входящие соединения на основном сокете
+            fclose($server);//master will not handle incoming connections on the main socket
             $masterClass = $this->config['master']['class'];
-            $master = new $masterClass ($service, $workers);//мастер будет обрабатывать сообщения от скриптов и пересылать их в воркеры
+            $master = new $masterClass ($service, $workers);//master will process messages from the script and send them to a worker
             if (!empty($this->config['master']['timer'])) {
                 $master->timer = $this->config['worker']['timer'];
             }
@@ -90,18 +90,18 @@ class Server
         $workers = array();
 
         for ($i=0; $i<$this->config['master']['workers']; $i++) {
-            //создаём парные сокеты, через них будут связываться мастер и воркер
+            //create a pair of sockets through which the master and the worker is to be linked
             $pair = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
 
-            $pid = pcntl_fork();//создаём форк
+            $pid = pcntl_fork();//create a fork
             if ($pid == -1) {
                 die("error: pcntl_fork\r\n");
-            } elseif ($pid) { //мастер
+            } elseif ($pid) { //master
                 fclose($pair[0]);
-                $workers[intval($pair[1])] = $pair[1];//один из пары будет в мастере
-            } else { //воркер
+                $workers[intval($pair[1])] = $pair[1];//one of the pair is in the master
+            } else { //worker
                 fclose($pair[1]);
-                $master = $pair[0];//второй в воркере
+                $master = $pair[0];//second of pair is in worker
                 break;
             }
         }
